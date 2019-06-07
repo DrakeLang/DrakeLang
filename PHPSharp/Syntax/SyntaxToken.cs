88e6b165123e965535eb112572999650f146945c
Niklas Gransjøen <niklasgransjoen@gmail.com>
2019-06-07 23:36:24 +0200

    Fixed NullReferenceException for empty text in SyntaxToken

--- PHPSharp/Syntax/SyntaxToken.cs
+++ PHPSharp/Syntax/SyntaxToken.cs
@@ -37,7 +37,7 @@ namespace PHPSharp.Syntax
         public int Position { get; }
         public string Text { get; }
         public object Value { get; }
-        public override TextSpan Span => new TextSpan(Position, Text.Length);
+        public override TextSpan Span => new TextSpan(Position, Text?.Length ?? 0);
 
         public override IEnumerable<SyntaxNode> GetChildren()
         {
