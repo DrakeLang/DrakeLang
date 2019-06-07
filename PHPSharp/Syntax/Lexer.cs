d0eeadbd5ef2754e4333d9535041aef1aac6d221
Niklas Gransjøen <niklasgransjoen@gmail.com>
2019-06-07 21:16:06 +0200

    Cleaned up lexer

--- PHPSharp/Syntax/Lexer.cs
+++ PHPSharp/Syntax/Lexer.cs
@@ -16,30 +16,16 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //------------------------------------------------------------------------------
 
-//------------------------------------------------------------------------------
-// PHP Sharp. Because PHP isn't good enough.
-// Copyright (C) 2019  Niklas Gransjøen
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see https://www.gnu.org/licenses/.
-//------------------------------------------------------------------------------
-
 namespace PHPSharp.Syntax
 {
     internal class Lexer
     {
         private readonly string _text;
+
         private int _position;
+        private int _start;
+        private SyntaxKind _kind;
+        private object _value;
 
         public Lexer(string text)
         {
@@ -64,70 +50,51 @@ namespace PHPSharp.Syntax
 
         public SyntaxToken Lex()
         {
-            int start = _position;
-
-            if (char.IsWhiteSpace(Current))
-            {
-                while (char.IsWhiteSpace(Current))
-                    Next();
-
-                int length = _position - start;
-                string whitespace = _text.Substring(start, length);
-                return new SyntaxToken(SyntaxKind.WhitespaceToken, start, whitespace, null);
-            }
-
-            if (_position >= _text.Length)
-                return new SyntaxToken(SyntaxKind.EndOfFileToken, _position, "\0", null);
-
-            if (char.IsDigit(Current))
-            {
-                while (char.IsDigit(Current))
-                    Next();
-
-                int length = _position - start;
-                string numberStr = _text.Substring(start, length);
-                if (!int.TryParse(numberStr, out int value))
-                    Diagnostics.ReportInvalidNumber(new TextSpan(start, length), _text, typeof(int));
-
-                return new SyntaxToken(SyntaxKind.NumberToken, start, numberStr, value);
-            }
-
-            if (char.IsLetter(Current))
-            {
-                while (char.IsLetter(Current))
-                    Next();
-
-                var length = _position - start;
-                string word = _text.Substring(start, length);
-                SyntaxKind kind = SyntaxFacts.GetKeywordKind(word);
-                return new SyntaxToken(kind, start, word, null);
-            }
+            _start = _position;
+            _kind = SyntaxKind.BadToken;
+            _value = null;
 
             switch (Current)
             {
+                case '\0':
+                    _kind = SyntaxKind.EndOfFileToken;
+                    break;
+
                 case '+':
-                    return new SyntaxToken(SyntaxKind.PlusToken, _position++, "+", null);
+                    _kind = SyntaxKind.PlusToken;
+                    _position++;
+                    break;
 
                 case '-':
-                    return new SyntaxToken(SyntaxKind.MinusToken, _position++, "-", null);
+                    _kind = SyntaxKind.MinusToken;
+                    _position++;
+                    break;
 
                 case '*':
-                    return new SyntaxToken(SyntaxKind.StarToken, _position++, "*", null);
+                    _kind = SyntaxKind.StarToken;
+                    _position++;
+                    break;
 
                 case '/':
-                    return new SyntaxToken(SyntaxKind.SlashToken, _position++, "/", null);
+                    _kind = SyntaxKind.SlashToken;
+                    _position++;
+                    break;
 
                 case '(':
-                    return new SyntaxToken(SyntaxKind.OpenParenthesisToken, _position++, "(", null);
+                    _kind = SyntaxKind.OpenParenthesisToken;
+                    _position++;
+                    break;
 
                 case ')':
-                    return new SyntaxToken(SyntaxKind.CloseParenthesisToken, _position++, ")", null);
+                    _kind = SyntaxKind.CloseParenthesisToken;
+                    _position++;
+                    break;
 
                 case '&':
                     if (LookAhead == '&')
                     {
+                        _kind = SyntaxKind.AmpersandAmpersandToken;
                         _position += 2;
-                        return new SyntaxToken(SyntaxKind.AmpersandAmpersandToken, start, "&&", null);
                     }
 
                     break;
@@ -135,34 +102,78 @@ namespace PHPSharp.Syntax
                 case '|':
                     if (LookAhead == '|')
                     {
+                        _kind = SyntaxKind.PipePipeToken;
                         _position += 2;
-                        return new SyntaxToken(SyntaxKind.PipePipeToken, start, "||", null);
                     }
-
                     break;
 
                 case '=':
                     if (LookAhead == '=')
                     {
+                        _kind = SyntaxKind.EqualsEqualsToken;
                         _position += 2;
-                        return new SyntaxToken(SyntaxKind.EqualsEqualsToken, start, "==", null);
                     }
                     else
-                        return new SyntaxToken(SyntaxKind.EqualsToken, _position++, "=", null);
+                    {
+                        _kind = SyntaxKind.EqualsToken;
+                        _position++;
+                    }
+                    break;
 
                 case '!':
                     if (LookAhead == '=')
                     {
+                        _kind = SyntaxKind.BangEqualsToken;
                         _position += 2;
-                        return new SyntaxToken(SyntaxKind.BangEqualsToken, start, "!=", null);
                     }
                     else
-                        return new SyntaxToken(SyntaxKind.BangToken, _position++, "!", null);
+                    {
+                        _position++;
+                        _kind = SyntaxKind.BangToken;
+                    }
+                    break;
+
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    ReadNumberToken();
+                    break;
+
+                case ' ':
+                case '\t':
+                case '\r':
+                case '\n':
+                    ReadWhitespace();
+                    break;
+
+                default:
+                    if (char.IsLetter(Current))
+                        ReadIdentifierOrKeyword();
+                    else if (char.IsWhiteSpace(Current))
+                        ReadWhitespace();
+                    else
+                    {
+                        Diagnostics.ReportBadCharacter(_position, Current);
+                        _position++;
+                    }
+                    break;
             }
 
-            Diagnostics.ReportBadCharacter(_position, Current);
-            string text = _text.Substring(_position, 1);
-            return new SyntaxToken(SyntaxKind.BadToken, _position++, text, null);
+            string text = SyntaxFacts.GetText(_kind);
+            if (text == null)
+            {
+                int length = _position - _start;
+                text = _text.Substring(_start, length);
+            }
+
+            return new SyntaxToken(_kind, _start, text, _value);
         }
 
         #endregion Methods
@@ -181,6 +192,42 @@ namespace PHPSharp.Syntax
         private void Next()
         {
             _position++;
+        }
+
+        private void ReadWhitespace()
+        {
+            while (char.IsWhiteSpace(Current))
+                Next();
+            int length = _position - _start;
+            string whitespace = _text.Substring(_start, length);
+
+            _kind = SyntaxKind.WhitespaceToken;
+            _value = whitespace;
+        }
+
+        private void ReadIdentifierOrKeyword()
+        {
+            while (char.IsLetter(Current))
+                Next();
+
+            int length = _position - _start;
+            string word = _text.Substring(_start, length);
+
+            _kind = SyntaxFacts.GetKeywordKind(word);
+        }
+
+        private void ReadNumberToken()
+        {
+            while (char.IsDigit(Current))
+                Next();
+
+            int length = _position - _start;
+            string text = _text.Substring(_start, length);
+            if (!int.TryParse(text, out int value))
+                Diagnostics.ReportInvalidNumber(new TextSpan(_start, length), _text, typeof(int));
+
+            _kind = SyntaxKind.NumberToken;
+            _value = value;
         }
 
         #endregion Private methods
